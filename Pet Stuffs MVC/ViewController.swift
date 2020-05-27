94e18cb836aacad94f36aa07b2ab88c6cae3474c
Vand <vandcarlosjunior@gmail.com>
2020-05-26 22:42:38 -0300

    Initial Commit

---
+++ Pet Stuffs MVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pet Stuffs MVC
+//
+//  Created by Vandcarlos Mouzinho Sandes Junior on 26/05/20.
+//  Copyright © 2020 Vandcarlos Mouzinho Sandes Junior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
